8ce8f3df8c5eef0555d9d4f90db7acced5fa2484
Nikosbatz <nikos.mpatz@outlook.com>
2025-03-11 23:55:14 +0200

    Splash Activity implementation

--- app/src/main/java/com/example/mytheaterapp/MainActivity.java
+++ app/src/main/java/com/example/mytheaterapp/MainActivity.java
@@ -25,10 +25,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         // Find Buttons Ids
-        imgPlay1 = findViewById(R.id.imgPlay1);
-        imgPlay2 = findViewById(R.id.imgPlay2);
-        button_chat = findViewById(R.id.button_chat);
+
 
         ArrayList<String> cast = new ArrayList<>(Arrays.asList("ena", "duo", "tria"));
         Play play1 = new Play("1o_ergo", cast, "comedy", "ewqeq qweqwe sadasfe qeewqewq asfas asdq weq", "gladiator" );
@@ -37,65 +36,7 @@ public class MainActivity extends AppCompatActivity {
         Play play2 = new Play("2o_ergo", cast, "comedy", "ewqeq qweqwe sadasfe qeewqewq asfas asdq weq", "gladiator" );
 
 
-        imgPlay1.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        ObjectAnimator.ofFloat(imgPlay1, "scaleX", 1.1f).setDuration(90).start();
-                        ObjectAnimator.ofFloat(imgPlay1, "scaleY", 1.1f).setDuration(90).start();
-                        imgPlay1.setAlpha(0.7f);
-                        break;
-                    case MotionEvent.ACTION_UP:
-                    case MotionEvent.ACTION_CANCEL:
-                        ObjectAnimator.ofFloat(imgPlay1, "scaleX", 1.0f).setDuration(100).start();
-                        ObjectAnimator.ofFloat(imgPlay1, "scaleY", 1.0f).setDuration(100).start();
-                        imgPlay1.setAlpha(1f);
-                        break;
-                }
-                return false;
-            }
-        });
 
-        imgPlay2.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        ObjectAnimator.ofFloat(imgPlay2, "scaleX", 1.1f).setDuration(90).start();
-                        ObjectAnimator.ofFloat(imgPlay2, "scaleY", 1.1f).setDuration(90).start();
-                        imgPlay2.setAlpha(0.7f);
-                        break;
-                    case MotionEvent.ACTION_UP:
-                    case MotionEvent.ACTION_CANCEL:
-                        ObjectAnimator.ofFloat(imgPlay2, "scaleX", 1.0f).setDuration(100).start();
-                        ObjectAnimator.ofFloat(imgPlay2, "scaleY", 1.0f).setDuration(100).start();
-                        imgPlay2.setAlpha(1f);
-                        break;
-                }
-                return false;
-            }
-        });
-
-        // Start the Play1 Activity
-        imgPlay1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), PlayInfo.class);
-                intent.putExtra("play", play1);
-                startActivity(intent);
-            }
-        });
-
-        // Start the Play2 Activity
-        imgPlay2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), PlayInfo.class);
-                intent.putExtra("play", play2);
-                startActivity(intent);
-            }
-        });
 
     }
 }
